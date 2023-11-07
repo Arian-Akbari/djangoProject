4d5e9fcaaa370ee6a62f43f2e5e90d96689609b0
“Arian” <arian09337332108@gmail.com>
2023-11-07 14:06:28 +0330

    learned basic things and how to pass dynamic data to our html files

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse('<h1> hey , welcome to my site </h1>')
+    contex = {
+        'name': 'patrik',
+        'age': '23',
+        'nationality': 'iran'
+
+    }
+    name = 'reza'
+    name2 = 'patrik'
+    return render(request, 'index.html', contex)
