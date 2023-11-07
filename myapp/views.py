645eaf095fa96bdf18433531e304f8002b9fde9f
“Arian” <arian09337332108@gmail.com>
2023-11-07 17:08:29 +0330

    preformed a user register in django with handling errors and adding the new created user to the data base

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Feature
 
@@ -13,3 +15,27 @@ def counter(request):
     amount = len(text.split())
     print(amount)
     return render(request, 'counter.html', {'amount': amount})
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repeat_password = request.POST['repeat_password']
+        if password == repeat_password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'password not the same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
