fbd533fdfbd4c6e5be14c5794aea4dd47c49ea4d
“Arian” <arian09337332108@gmail.com>
2023-11-07 15:18:04 +0330

    build a simple tool to count words with post method

--- myapp/views.py
+++ myapp/views.py
@@ -12,3 +12,10 @@ def index(request):
     name = 'reza'
     name2 = 'patrik'
     return render(request, 'index.html', contex)
+
+
+def counter(request):
+    text = request.POST['text']
+    amount = len(text.split())
+    print(amount)
+    return render(request, 'counter.html', {'amount': amount})
